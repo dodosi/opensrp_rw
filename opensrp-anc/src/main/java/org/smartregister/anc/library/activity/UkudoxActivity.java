280eae3c6cd82b0c12f1ada41bc71c19791470e9
ericnn250 <38242660+ericnn250@users.noreply.github.com>
2022-02-15 18:59:26 +0200

    dashboard statting

--- opensrp-anc/src/main/java/org/smartregister/anc/library/activity/UkudoxActivity.java
+++ opensrp-anc/src/main/java/org/smartregister/anc/library/activity/UkudoxActivity.java
@@ -2,28 +2,39 @@ package org.smartregister.anc.library.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
 import org.smartregister.anc.library.R;
+import org.smartregister.anc.library.adapter.HomeAdapter;
 import org.smartregister.anc.library.adapter.LibraryContentAdapter;
 import org.smartregister.anc.library.adapter.UkudoxAdapter;
+import org.smartregister.anc.library.domain.HomeItem;
 import org.smartregister.anc.library.model.LibraryContent;
 import org.smartregister.anc.library.model.UkudoxContent;
 import org.smartregister.anc.library.util.ConstantsUtils;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class UkudoxActivity extends AppCompatActivity {
     protected Toolbar mToolbar;
     private RecyclerView contentLayout;
     Button go;
+    private DatePickerDialog datePickerDialog;
+    private Button dateButton;
+    private Button dateButton2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,6 +56,24 @@ public class UkudoxActivity extends AppCompatActivity {
     private void setUpViews() {
         mToolbar = findViewById(R.id.library_toolbar);
         contentLayout = findViewById(R.id.layout_attach_recycler_view);
+        initDatePicker();
+        initDatePicker2();
+        dateButton = findViewById(R.id.datePickerButton);
+        dateButton.setText(getTodaysDate());
+        dateButton2 = findViewById(R.id.datePickerButton2);
+        dateButton2.setText(getTodaysDate());
+        dateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDatePicker(view);
+            }
+        });
+        dateButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDatePicker2(view);
+            }
+        });
     }
     private void gotToHomeRegister(boolean remote) {
         Intent intent = new Intent(this, BaseHomeRegisterActivity.class);
@@ -52,9 +81,10 @@ public class UkudoxActivity extends AppCompatActivity {
         startActivity(intent);
     }
     private void attachLayoutContentAdapter() {
-        UkudoxAdapter libraryContentAdapter = new UkudoxAdapter(getLibraryContent(), this);
+        HomeAdapter libraryContentAdapter = new HomeAdapter( getHomeContent(),new HomeActionHandler());
         libraryContentAdapter.notifyDataSetChanged();
-        contentLayout.setLayoutManager(new LinearLayoutManager(this));
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(UkudoxActivity.this, 2);
+        contentLayout.setLayoutManager(mLayoutManager);
         contentLayout.setAdapter(libraryContentAdapter);
     }
 
@@ -74,5 +104,145 @@ public class UkudoxActivity extends AppCompatActivity {
             //libraryContents.add(reportTab);
         }
         return libraryContents;
+    }
+    private  List<HomeItem> getHomeContent(){
+        List<HomeItem> homeItems= new ArrayList<>();
+
+        HomeItem dueContactToday=new HomeItem();
+        dueContactToday.setTitle("Due Contact today");
+        dueContactToday.setNumber(23);
+        dueContactToday.setBackground(R.drawable.profile_bg);
+
+        homeItems.add(dueContactToday);
+
+        HomeItem refered= new HomeItem();
+        refered.setTitle("Woman referred today");
+        refered.setBackground(R.drawable.profile_bg);
+        refered.setNumber(0);
+
+        homeItems.add(refered);
+
+        HomeItem  dangersigns= new HomeItem();
+        dangersigns.setTitle("Mother with Danger signs");
+        dangersigns.setNumber(0);
+        dangersigns.setBackground(R.drawable.profile_bg);
+
+        homeItems.add(dangersigns);
+        HomeItem processed =new HomeItem();
+        processed.setTitle("Processed case");
+        processed.setNumber(6);
+        processed.setBackground(R.drawable.profile_bg);
+        homeItems.add(processed);
+
+        return homeItems;
+
+    }
+    private String getTodaysDate()
+    {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        month = month + 1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        return getMonthFormat(month) + " " + day + " " + year;
+    }
+
+    private String getMonthFormat(int month) {
+
+        if(month == 1)
+            return "JAN";
+        if(month == 2)
+            return "FEB";
+        if(month == 3)
+            return "MAR";
+        if(month == 4)
+            return "APR";
+        if(month == 5)
+            return "MAY";
+        if(month == 6)
+            return "JUN";
+        if(month == 7)
+            return "JUL";
+        if(month == 8)
+            return "AUG";
+        if(month == 9)
+            return "SEP";
+        if(month == 10)
+            return "OCT";
+        if(month == 11)
+            return "NOV";
+        if(month == 12)
+            return "DEC";
+
+        //default should never happen
+        return "JAN";
+    }
+    private void initDatePicker()
+    {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener()
+        {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day)
+            {
+                month = month + 1;
+                String date = getMonthFormat(month) + " " + day + " " + year;
+                dateButton.setText(date);
+            }
+        };
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+
+        datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
+        datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+
+    }
+    public void openDatePicker(View view)
+    {
+        datePickerDialog.show();
+    }
+    private void initDatePicker2() {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener()
+        {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day)
+            {
+                month = month + 1;
+                String date = getMonthFormat(month) + " " + day + " " + year;
+                dateButton2.setText(date);
+            }
+        };
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+
+        datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
+        datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+//        Date date = sdf.parse(dateButton.getText().toString());
+//        long millis = date.getTime();
+//        datePickerDialog.getDatePicker().setMinDate(millis);
+    }
+    public void openDatePicker2(View view)
+    {
+        datePickerDialog.show();
+    }
+    private class HomeActionHandler implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            int i = view.getId();
+            if (i == R.id.card_layout) {
+                Toast.makeText(UkudoxActivity.this, "High Clicked", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
