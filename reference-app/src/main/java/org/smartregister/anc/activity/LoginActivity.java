80c97575660c13b526b9146922c24f4b799856a2
Dieudonne <ukudox@gmail.com>
2023-11-09 12:57:08 +0200

    Nw changes made Profile

--- reference-app/src/main/java/org/smartregister/anc/activity/LoginActivity.java
+++ reference-app/src/main/java/org/smartregister/anc/activity/LoginActivity.java
@@ -1,9 +1,15 @@
 package org.smartregister.anc.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.apache.commons.lang3.StringUtils;
 import org.greenrobot.eventbus.Subscribe;
@@ -16,9 +22,13 @@ import org.smartregister.anc.library.event.ViewConfigurationSyncCompleteEvent;
 import org.smartregister.anc.library.util.ConstantsUtils;
 import org.smartregister.anc.library.util.Utils;
 import org.smartregister.anc.presenter.LoginPresenter;
+import org.smartregister.repository.dao.LocationDaoImpl;
+import org.smartregister.security.SecurityHelper;
 import org.smartregister.task.SaveTeamLocationsTask;
 import org.smartregister.view.activity.BaseLoginActivity;
 import org.smartregister.view.contract.BaseLoginContract;
+
+import java.util.Locale;
 
 import timber.log.Timber;
 
@@ -26,7 +36,8 @@ import timber.log.Timber;
  * Created by ndegwamartin on 21/06/2018.
  */
 public class LoginActivity extends BaseLoginActivity implements BaseLoginContract.View {
-
+    EditText usernameField,passwordField;
+    Button loginBtn;
     @Override
     protected void onResume() {
         super.onResume();
@@ -40,15 +51,39 @@ public class LoginActivity extends BaseLoginActivity implements BaseLoginContrac
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setUpViews();
+
     }
+//
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        if(!getUsername().equals("")){
+//            loginBtn.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    if(validateCredentials(usernameField.getText().toString(),passwordField.getText().toString()) ){
+//                        Log.d("CLICKED","MY BUTTON");
+//                        gotToHomeRegister(false);
+//
+//                    }
+//                }
+//            });
+//        }else{
+//            Log.d("UKUDOX","NOT FROM LOCAL 11");
+////        }
+//    }
 
     private void setUpViews() {
         TextView formReleaseTextView = findViewById(R.id.manifest_text_view);
+        usernameField= findViewById(R.id.login_user_name_edit_text);
+        passwordField= findViewById(R.id.login_password_edit_text);
+        loginBtn= findViewById(R.id.login_login_btn);
         if (StringUtils.isNotBlank(new Utils().getManifestVersion(this))) {
             formReleaseTextView.setText(new Utils().getManifestVersion(this));
         } else {
             formReleaseTextView.setVisibility(View.GONE);
         }
+
     }
 
     @Override
@@ -58,8 +93,8 @@ public class LoginActivity extends BaseLoginActivity implements BaseLoginContrac
 
     @Override
     protected void initializePresenter() {
-        mLoginPresenter = new LoginPresenter(this);
-    }
+       mLoginPresenter = new LoginPresenter(this);
+     }
 
     @Override
     public void goToHome(boolean remote) {
@@ -67,8 +102,9 @@ public class LoginActivity extends BaseLoginActivity implements BaseLoginContrac
 //            Utils.startAsyncTask(new SaveTeamLocationsTask(), null);
             new SaveTeamLocationsTask().execute();
         }
-
-        if (mLoginPresenter.isServerSettingsSet()) {
+       if (mLoginPresenter.isServerSettingsSet()) {
+            Log.d("UKUDOX","FROM REMOTE");
+            setCredentials(usernameField.getText().toString(),passwordField.getText().toString());
             gotToHomeRegister(remote);
         } else {
             goToSiteCharacteristics(remote);
@@ -78,7 +114,7 @@ public class LoginActivity extends BaseLoginActivity implements BaseLoginContrac
     }
 
     private void gotToHomeRegister(boolean remote) {
-//        Intent intent = new Intent(this, BaseHomeRegisterActivity.class);
+       //Intent intent = new Intent(this, BaseHomeRegisterActivity.class);
         //ukudox stuff
         Intent intent = new Intent(this, UkudoxActivity.class);
         intent.putExtra(ConstantsUtils.IntentKeyUtils.IS_REMOTE_LOGIN, remote);
@@ -98,5 +134,35 @@ public class LoginActivity extends BaseLoginActivity implements BaseLoginContrac
             mLoginPresenter.processViewCustomizations();
 
         }
+    }
+    public  void setCredentials(String username,String password){
+        SharedPreferences sharedPreferences=getSharedPreferences("credentials", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor=sharedPreferences.edit();
+        editor.putString("username",username);
+        editor.putString("password",password);
+        editor.commit();
+    }
+    public boolean validateCredentials(String newUsername,String newPassword){
+        SharedPreferences sharedPreferences=getSharedPreferences("credentials",Context.MODE_PRIVATE);
+        String username="";
+        String password="";
+        if(sharedPreferences.contains("username")&&(sharedPreferences.contains("password"))){
+            username=sharedPreferences.getString("username","");
+            password=sharedPreferences.getString("password","");
+            Log.d("CRED",username+" "+password);
+        }else{
+            Log.d("CRED","Nothing saved "+username+" PASS "+password);
+        }
+        return newUsername.equals(username)&&newPassword.equals(password);
+    }
+    public String getUsername(){
+        SharedPreferences sharedPreferences=getSharedPreferences("credentials",Context.MODE_PRIVATE);
+        String username="";
+        if(sharedPreferences.contains("username")){
+            username=sharedPreferences.getString("username","");
+        }else{
+            Log.d("USERNAME","Nothing saved "+username);
+        }
+        return username;
     }
 }
